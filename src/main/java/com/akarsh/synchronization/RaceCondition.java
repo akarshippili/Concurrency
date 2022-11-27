325140a671aeff7e834a33638134d4f42097838b
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-11-27 21:20:50 +0530

    Create RaceCondition.java

---
+++ src/main/java/com/akarsh/synchronization/RaceCondition.java
@@ -0,0 +1,56 @@
+package com.akarsh.synchronization;
+
+
+class Counter {
+    private int count = 0;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void incrementCount() {
+        count++;
+    }
+}
+
+
+public class RaceCondition {
+
+    public static void main(String[] args) {
+        process();
+    }
+    public static void process() {
+
+        // in heap - shared memory / resource
+        Counter counter = new Counter();
+
+        Runnable run = () -> {
+            for (int i = 0; i <100000; i++) {
+                // not an atomic operation
+                // context switch can happen in between - leads to race condition
+                // non-deterministic
+                counter.incrementCount();
+            }
+        };
+
+        Thread t1 = new Thread(run);
+        Thread t2 = new Thread(run);
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(counter.getCount());
+
+    }
+}
